5652a2def5cedde9967047d3a875866b2a981b36
Nathan Jen <njen@andrew.cmu.edu>
2020-12-06 12:01:01 -0500

    moved eda buttons to side and improved styling of mystery section

--- components/answer.js
+++ components/answer.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class Answer extends React.Component {
   render() {
-    const {correct, show} = this.props
+    const {correct, show, children} = this.props
 
     return (
       <div>
-        {correct && show && <div>Correct!</div>}
-        {!correct && show && <div>Wrong :(</div>}
+        {correct && show && <div className='correct'>Correct! ✅</div>}
+        {!correct && show && <div className='wrong'>Wrong X</div>}
+        {show && children}
       </div>
     )
   }
