ddcd4f854d307cea97edb199b2511635e538a2a3
Nathan Jen <njen@andrew.cmu.edu>
2020-12-07 18:49:50 -0500

    made it easier to pass link prop to iframe

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Iframe({ iframe }) {
-  const iframe = iframe
+export default function Iframe({ link }) {
+  const iframe = `<iframe src=${link} width="100%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>`
   return (
     <div dangerouslySetInnerHTML={ {__html:  iframe}} />
   )
