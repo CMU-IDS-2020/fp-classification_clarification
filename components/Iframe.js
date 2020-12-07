2bf3adaa3aefd1eb5bdaeaf1c13057f0c48ad7de
Juliette Wong <jnwong@andrew.cmu.edu>
2020-12-07 00:15:28 -0500

    logreg writeup
    Started adding to logreg

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Iframe(props) {
-  const iframe = '<iframe src="https://www.desmos.com/calculator/plfizuubiv?embed" width="500px" height="500px" style="border: 1px solid #ccc" frameborder=0></iframe>'
+  const iframe = '<iframe src="https://www.desmos.com/calculator/plfizuubiv?embed" width="100%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>'
   return (
     <div dangerouslySetInnerHTML={ {__html:  iframe}} />
   )
