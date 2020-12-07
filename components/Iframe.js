1ff25840c49c67e7812080a4a3e5057645d870f7
Nathan Jen <njen@andrew.cmu.edu>
2020-12-07 18:23:09 -0500

    iframe component now accepting iframe prop

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Iframe(props) {
-  const iframe = '<iframe src="https://www.desmos.com/calculator/plfizuubiv?embed" width="100%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>'
+export default function Iframe({ iframe }) {
+  const iframe = iframe
   return (
     <div dangerouslySetInnerHTML={ {__html:  iframe}} />
   )
